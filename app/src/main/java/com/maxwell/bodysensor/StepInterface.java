c98dd673d0715bb7394253ede30becfed2256688
Nikita Sikka <nikita.sikka@gmail.com>
2015-08-24 12:53:25 +0530

    PR2

---
+++ app/src/main/java/com/maxwell/bodysensor/StepInterface.java
@@ -0,0 +1,10 @@
+package com.maxwell.bodysensor;
+
+/**
+ * Created by ryanhsueh on 15/5/15.
+ */
+public interface StepInterface {
+    void previousStep();
+    void nextStep();
+    void finish();
+}
